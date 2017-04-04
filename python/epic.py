425811d5e0ee7e9a29140c1f7fcd1b5476d1300b
hollisneel <13ncn@uga.edu>
2017-04-03 23:15:50 -0400

    Added feature detection

--- python/epic.py
+++ python/epic.py
@@ -8,18 +8,20 @@ class Scene:
     def __init__(self,imagePathDir):
         files = os.listdir(imagePathDir)
         finalArray = []
-        for name in files:
+        # Assume num of files starts at 1
+        for i in range(1,len(files)+1):
             # final array is of format [image][row][col][rgb]
-            finalArray.append(cv2.imread(imagePathDir+name))    
+            finalArray.append(cv2.imread(imagePathDir+str(i)+".jpg"))    
 
         self.numImages = len(files)
-        self.nrows = len(finalArray[0])
-        self.ncols = len(finalArray[0][0])
-        self.depth = len(finalArray[0][0][0])
-
-        self.slice = 0
-        self.stackArray = finalArray
-
+        self.nrows     = len(finalArray[0])
+        self.ncols     = len(finalArray[0][0])
+        self.depth     = len(finalArray[0][0][0])
+        self.image     = 0
+        self.slice     = 0
+        self.stackArray= finalArray
+        self.boolArray = []
+        self.boolSlice = 0
 
 
     def setSlice(self,rowHeight):
@@ -42,15 +44,59 @@ class Scene:
             cv2.imshow("Image",self.slice) 
             cv2.waitKey()
 
-    def showImage(self,imageNumber):
+    def setImage(self,imageNumber):
         img = numpy.zeros((self.nrows, self.ncols, self.depth),dtype='uint8')
 
         for row in range(self.nrows):
             for col in range(self.ncols):
                 for depth in range(self.depth):
                     img[row][col][depth] = self.stackArray[imageNumber][row][col][depth]
-        cv2.imshow("Image #"+str(imageNumber),img)
-        cv2.waitKey() 
+        self.image = img
+    
+
+    def showImage(self):
+        cv2.imshow("Image",self.image)
+        cv2.waitKey()
+
+    def saveSlice(self,name):
+        cv2.imwrite(self.slice)
+
+    def cornerDetect(self,r):
+        fast = cv2.FastFeatureDetector()
+        final = []
+        for image in range(self.numImages):
+            bArray = numpy.zeros((self.nrows,self.ncols,self.depth),dtype='uint8')
+
+            self.setImage(image)
+            kp = fast.detect(self.image,None)   
+            for pt in kp:
+                for x_change in range(-r,r):
+                    for y_change in range(-r,r):
+                        bArray[int(pt.pt[1])+y_change][int(pt.pt[0])+x_change][0] = 255
+                        bArray[int(pt.pt[1])+y_change][int(pt.pt[0])+x_change][1] = 255
+                        bArray[int(pt.pt[1])+y_change][int(pt.pt[0])+x_change][2] = 255
+            final.append(bArray)
+        self.boolArray = final
+
+    def featureSlice(self,rowHeight):
+        ncols = self.ncols
+        depth = self.depth
+        numImages = self.numImages
+
+        array = numpy.zeros((numImages,ncols,depth),dtype='uint8')
+
+        for row in range(numImages):
+            for col in range(ncols):
+                for dep in range(depth):
+                    array[row][col][dep] = self.boolArray[row][rowHeight][col][dep]
+
+        self.featureSlice = array
+
+        
+
+
+
+
 
 def showColor(filepath):
     z = open(filepath)
